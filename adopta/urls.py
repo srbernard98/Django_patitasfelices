022b9b5bf2edc6fa749c41f7c4dd51cea7f97e85
srbernard98 <bern.rojas@duocuc.cl>
2024-07-17 22:21:58 -0400

    nav

--- adopta/urls.py
+++ adopta/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index 
+from . import views
 
 urlpatterns = [
     path('', index, name='index'),
+    path('inscribe/', views.inscribe, name='inscribe'),
+    path('sobre_nosotros/', views.sobre_nosotros, name='sobre_nosotros'),
+    path('adopta/', views.adopta, name='adopta'),
+    path('registro/', views.registro, name='registro'),
 ]
--- adopta/views.py
+++ adopta/views.py
@@ -4,3 +4,24 @@ from django.shortcuts import render
 def index(request):
 
     return render(request,'adopta/index.html')
+
+
+# Create your views here.
+def inscribe(request):
+
+    return render(request,'adopta/inscribe.html')
+
+# Create your views here.
+def sobre_nosotros(request):
+
+    return render(request,'adopta/Sobre Nosotros.html')
+
+
+def adopta(request):
+
+    return render(request,'adopta/adopta.html')
+
+def registro(request):
+
+    return render(request,'adopta/registro.html')
+
